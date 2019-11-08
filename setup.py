2f825c2a1da828611aa944ca0d87d6c12b26f86a
Fangfang Yan <30477318+fangfang0906@users.noreply.github.com>
2019-11-08 14:54:38 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="DrivAER",
+    version="0.0.1",
+    description="Identify driving transcriptional regulator in single cell embeddings",
+    author='Lukas Simon, Fangfang Yan',
+    author_email="Lukas.Simon@uth.tmc.edu",
+    packages=['DrivAER'],
+    install_requires=['sklearn','scanpy','anndata',
+                      'pandas','seaborn','matplotlib','dca','tensorflow'
+                      ],
+    package_data={'DrivAER': ['data/*.txt','annotations/*.gmt']}
+)
