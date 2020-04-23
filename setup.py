d8cd45cffbb94427b133cf8a6891da2189dea476
lkmklsmn <lkmklsmn@gmail.com>
2020-04-23 11:31:05 -0600

    update email

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import setuptools
 setuptools.setup(
     name="DrivAER",
     version="0.0.1",
-    description="Identify driving transcriptional regulator in single cell embeddings",
+    description="Identify driving transcriptional regulators in single cell embeddings",
     author='Lukas Simon, Fangfang Yan',
-    author_email="Lukas.Simon@uth.tmc.edu",
+    author_email="lkmklsmn@gmail.com",
     packages=['DrivAER'],
     install_requires=['sklearn','scanpy','anndata',
                       'pandas','seaborn','matplotlib','dca','tensorflow<=1.15.2'
