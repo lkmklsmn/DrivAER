f3da7f4a4ad77388c8a920dba0e75e499b5aa34a
lkmklsmn <lkmklsmn@gmail.com>
2020-10-01 17:40:58 -0600

    change tensorflow from 1.15.2 to 1.15.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     author_email="lkmklsmn@gmail.com",
     packages=['DrivAER'],
     install_requires=['sklearn','scanpy','anndata', 'keras==2.3.1',
-                      'pandas','seaborn','matplotlib','dca','tensorflow<=1.15.2'
+                      'pandas','seaborn','matplotlib','dca','tensorflow<=1.15.4'
                       ],
     package_data={'DrivAER': ['data/*.txt','annotations/*.gmt','annotations/*.tsv']}
 )
